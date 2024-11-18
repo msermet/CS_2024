fe2e6d57ced0fdd28a11cdd315f8e9e5882e0d05
sermetm <eargaerg@aergaerg.com>
2024-11-18 15:35:06 +0100

    question 4 finie

--- mail-test.php
+++ mail-test.php
@@ -1,26 +1,40 @@
 <?php
 include "vendor/autoload.php";
 use PHPMailer\PHPMailer\PHPMailer; //Obligatoire pour avoir l’objet phpmailer qui marche
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->Host = '127.0.0.1';
-$mail->Port = 1025; //Port non crypté
-$mail->SMTPAuth = false; //Pas d’authentification
-$mail->SMTPAutoTLS = false; //Pas de certificat TLS
-$mail->setFrom('test@labruleriecomtoise.fr', 'admin');
-$mail->addAddress('client@labruleriecomtoise.fr', 'Mon client');
-if ($mail->addReplyTo('test@labruleriecomtoise.fr', 'admin')) {
-    $mail->Subject = 'Objet : Bonjour !';
-    $mail->isHTML(false);
-    $mail->Body = "Corps du message pour mon client :)";
+//$mail = new PHPMailer;
+//$mail->isSMTP();
+//$mail->Host = '127.0.0.1';
+//$mail->Port = 1025; //Port non crypté
+//$mail->SMTPAuth = false; //Pas d’authentification
+//$mail->SMTPAutoTLS = false; //Pas de certificat TLS
+//$mail->setFrom('test@labruleriecomtoise.fr', 'admin');
+//$mail->addAddress('client@labruleriecomtoise.fr', 'Mon client');
+//if ($mail->addReplyTo('test@labruleriecomtoise.fr', 'admin')) {
+//    $mail->Subject = 'Objet : Bonjour !';
+//    $mail->isHTML(false);
+//    $mail->Body = "Corps du message pour mon client :)";
+//
+//    if (!$mail->send()) {
+//        $msg = 'Désolé, quelque chose a mal tourné. Veuillez réessayer plus tard.';
+//    } else {
+//        $msg = 'Message envoyé ! Merci de nous avoir contactés.';
+//    }
+//} else {
+//    $msg = 'Il doit manquer qqc !';
+//}
+//echo $msg;
 
-    if (!$mail->send()) {
-        $msg = 'Désolé, quelque chose a mal tourné. Veuillez réessayer plus tard.';
-    } else {
-        $msg = 'Message envoyé ! Merci de nous avoir contactés.';
-    }
-} else {
-    $msg = 'Il doit manquer qqc !';
+
+
+function passgen1($nbChar) {
+$chaine = "mnoTUzS5678kVvwxy9WXYZRNCDEFrslq41GtuaHIJKpOPQA23LcdefghiBMbj0";
+srand((double)microtime() * 1000000);
+$pass = '';
+for ($i = 0; $i < $nbChar; $i++) {
+$pass .= $chaine[rand() % strlen($chaine)];
 }
-echo $msg;
+return $pass;
+}
+
+echo passgen1(10);
 ?>
