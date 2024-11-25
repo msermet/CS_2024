126ea2311f78ac31cc2cbca1e436c6f3db961fdf
sermetm <eargaerg@aergaerg.com>
2024-11-25 15:49:57 +0100

    4 finie

--- Controleur/Controleur_visiteur.php
+++ Controleur/Controleur_visiteur.php
@@ -45,6 +45,7 @@ switch ($action) {
         $Vue->addToCorps(new Vue_Mail_ReinitMdp());
 
         break;
+
     case "Se connecter" :
         if (isset($_REQUEST["compte"]) and isset($_REQUEST["password"])) {
             //Si tous les paramètres du formulaire sont bons
@@ -54,63 +55,60 @@ switch ($action) {
             if ($utilisateur != null) {
                 //error_log("utilisateur : " . $utilisateur["idUtilisateur"]);
                 if ($utilisateur["desactiver"] == 0) {
-                    if (isset($_SESSION["motdepasseProv"])) {
-                        if ($_SESSION["motdepasseProv"]==$utilisateur["motDePasse"]) {
-                            $Vue->addToCorps(new Vue_Utilisateur_Changement_MDP("", "Gerer_monCompte"));
-                            session_destroy();
-                        }
-                    } else {
-                        if ($_REQUEST["password"] == $utilisateur["motDePasse"]) {
-                            $_SESSION["idUtilisateur"] = $utilisateur["idUtilisateur"];
-                            //error_log("idUtilisateur : " . $_SESSION["idUtilisateur"]);
-                            $_SESSION["idCategorie_utilisateur"] = $utilisateur["idCategorie_utilisateur"];
-                            // echo "idCategorie_utilisateur : " . $_SESSION["idCategorie_utilisateur"];
-                            //error_log("idCategorie_utilisateur : " . $_SESSION["idCategorie_utilisateur"]);
-                            //  var_dump($utilisateur);
+                    if ($_REQUEST["password"] == $utilisateur["motDePasse"] ||$_SESSION["motdepasseProv"]==$utilisateur["motDePasse"]) {
+                        $_SESSION["idUtilisateur"] = $utilisateur["idUtilisateur"];
+                        //error_log("idUtilisateur : " . $_SESSION["idUtilisateur"]);
+                        $_SESSION["idCategorie_utilisateur"] = $utilisateur["idCategorie_utilisateur"];
+                        // echo "idCategorie_utilisateur : " . $_SESSION["idCategorie_utilisateur"];
+                        //error_log("idCategorie_utilisateur : " . $_SESSION["idCategorie_utilisateur"]);
+                        //  var_dump($utilisateur);
 
 
-                            //ici
-                            if (Modele_Utilisateur::Utilisateur_Select_RGPD($_SESSION["idUtilisateur"]) == 0) {
-                                include "./Controleur/Controleur_AccepterRGPD.php";
-                                //                            $msgError = "RGPD non accepté";
-                                //                            $Vue->addToCorps(new Vue_Connexion_Formulaire_client($msgError));
-                                //                            $erreur = true;
-                            } else
-                                switch ($utilisateur["idCategorie_utilisateur"]) {
-                                    case 1:
-                                        $_SESSION["typeConnexionBack"] = "administrateurLogiciel"; //Champ inutile, mais bien pour voir ce qu'il se passe avec des étudiants !
-                                        $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
-                                        break;
-                                    case 2:
-                                        $_SESSION["typeConnexionBack"] = "gestionnaireCatalogue";
-                                        $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
-                                        $Vue->addToCorps(new \App\Vue\Vue_AfficherMessage("Bienvenue " . $_REQUEST["compte"]));
-                                        break;
-                                    case 3:
-                                        $_SESSION["typeConnexionBack"] = "entrepriseCliente";
-                                        //error_log("idUtilisateur : " . $_SESSION["idUtilisateur"]);
-                                        $_SESSION["idEntreprise"] = Modele_Entreprise::Entreprise_Select_Par_IdUtilisateur($_SESSION["idUtilisateur"])["idEntreprise"];
-                                        include "./Controleur/Controleur_Gerer_Entreprise.php";
-                                        break;
-                                    case 4:
-                                        $_SESSION["typeConnexionBack"] = "salarieEntrepriseCliente";
-                                        $_SESSION["idSalarie"] = $utilisateur["idUtilisateur"];
-                                        $_SESSION["idEntreprise"] = Modele_Salarie::Salarie_Select_byId($_SESSION["idUtilisateur"])["idEntreprise"];
-                                        include "./Controleur/Controleur_Catalogue_client.php";
-                                        break;
-                                    case 5:
-                                        $_SESSION["typeConnexionBack"] = "commercialCafe";
-                                        $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
-                                        break;
-                                }
+                        //ici
+                        if (Modele_Utilisateur::Utilisateur_Select_RGPD($_SESSION["idUtilisateur"]) == 0) {
+                            include "./Controleur/Controleur_AccepterRGPD.php";
+                            //                            $msgError = "RGPD non accepté";
+                            //                            $Vue->addToCorps(new Vue_Connexion_Formulaire_client($msgError));
+                            //                            $erreur = true;
+                        } else
+                            if (isset($_SESSION["motdepasseProv"])) {
+                                include "./Controleur/Controleur_Gerer_monCompte.php";
+                            }
 
-                        } else {//mot de passe pas bon
-                            $msgError = "Mot de passe erroné";
+                            switch ($utilisateur["idCategorie_utilisateur"]) {
+                                case 1:
+                                    $_SESSION["typeConnexionBack"] = "administrateurLogiciel"; //Champ inutile, mais bien pour voir ce qu'il se passe avec des étudiants !
+                                    $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
+                                    break;
+                                case 2:
+                                    $_SESSION["typeConnexionBack"] = "gestionnaireCatalogue";
+                                    $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
+                                    $Vue->addToCorps(new \App\Vue\Vue_AfficherMessage("Bienvenue " . $_REQUEST["compte"]));
+                                    break;
+                                case 3:
+                                    $_SESSION["typeConnexionBack"] = "entrepriseCliente";
+                                    //error_log("idUtilisateur : " . $_SESSION["idUtilisateur"]);
+                                    $_SESSION["idEntreprise"] = Modele_Entreprise::Entreprise_Select_Par_IdUtilisateur($_SESSION["idUtilisateur"])["idEntreprise"];
+                                    include "./Controleur/Controleur_Gerer_Entreprise.php";
+                                    break;
+                                case 4:
+                                    $_SESSION["typeConnexionBack"] = "salarieEntrepriseCliente";
+                                    $_SESSION["idSalarie"] = $utilisateur["idUtilisateur"];
+                                    $_SESSION["idEntreprise"] = Modele_Salarie::Salarie_Select_byId($_SESSION["idUtilisateur"])["idEntreprise"];
+                                    include "./Controleur/Controleur_Catalogue_client.php";
+                                    break;
+                                case 5:
+                                    $_SESSION["typeConnexionBack"] = "commercialCafe";
+                                    $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
+                                    break;
+                            }
+                    } else {//mot de passe pas bon
+                        $msgError = "Mot de passe erroné";
 
-                            $Vue->addToCorps(new Vue_Connexion_Formulaire_client($msgError));
+                        $Vue->addToCorps(new Vue_Connexion_Formulaire_client($msgError));
 
-                        }
                     }
+
                 } else {
                     $msgError = "Compte désactivé";
 
